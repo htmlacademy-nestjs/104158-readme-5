65b12358cf0f736f6884bd6f262a7d54b995d764
Kirill Dvoryaninov <kdvoryaninov@yandex.ru>
2023-12-14 10:03:00 +0300

    Rest API

---
+++ project/apps/account/src/app/authentication/authentication.constant.ts
@@ -0,0 +1,3 @@
+export const AUTH_USER_EXISTS = 'Пользователь с этим адресом электронной почты существует';
+export const AUTH_USER_NOT_FOUND = 'Пользователь не найден';
+export const AUTH_USER_PASSWORD_WRONG = 'Пароль пользователя неправильный';
---
+++ project/shared/shared-types/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/post.interface';
+export * from './lib/user-role.enum';
+export * from './lib/user.interface';
+export * from './lib/auth-user.interface';
